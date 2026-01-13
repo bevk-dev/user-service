743d079bbc9f09d46035f68f031844f06d5c5de0
UBevk <bevk.urh@gmail.com>
2026-01-13 09:26:39 +0100

    m2, embedded kafka

--- src/test/java/com/shopsync/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/shopsync/user_service/UserServiceApplicationTests.java
@@ -2,8 +2,15 @@ package com.shopsync.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;      
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(
+    partitions = 1, 
+    bootstrapServersProperty = "spring.kafka.bootstrap-servers" 
+)
 class UserServiceApplicationTests {
 
 	@Test
